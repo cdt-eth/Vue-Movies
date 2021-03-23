549cdfc82425472aae50893afd4c88f59ac5590c
christianturner <christiandavidphoto@gmail.com>
2021-03-23 14:22:50 -0400

    response body data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,20 @@
       <input type="submit" placeholder="Search" />
     </form>
 
-    <div class="movies-list">MOVIES</div>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imbdID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detial">
+            <p class="y">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,6 +58,8 @@ export default {
           .then((response) => response.json())
           .then((data) => {
             console.log(data);
+            movies.value = data.Search;
+            search.value = "";
           });
         console.log(search.value);
       }
@@ -107,7 +122,8 @@ export default {
         width: 100%;
         color: white;
         background-color: #496583;
-        font-size: 24px;
+        font-size: 18px;
+        text-align: center;
         padding: 10px 16px;
         border-radius: 8px;
         margin-bottom: 15px;
@@ -118,7 +134,7 @@ export default {
         }
 
         &:focus {
-          box-shadow: 0px 3px 6px;
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
         }
       }
 
