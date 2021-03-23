c15a20f54ed1a00f3078509a089ea497b9524222
christianturner <christiandavidphoto@gmail.com>
2021-03-23 14:35:25 -0400

    movie result component styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,8 +34,8 @@
             <img :src="movie.Poster" alt="Movie Poster" />
             <div class="type">{{ movie.Type }}</div>
           </div>
-          <div class="detial">
-            <p class="y">{{ movie.Year }}</p>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
             <h3>{{ movie.Title }}</h3>
           </div>
         </router-link>
@@ -151,6 +151,61 @@ export default {
 
         &:active {
           background-color: #3b8070;
+        }
+      }
+    }
+  }
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: white;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #aaa;
+            font-size: 14px;
+          }
+          h3 {
+            color: white;
+            font-weight: 600;
+            font-size: 18px;
+          }
         }
       }
     }
